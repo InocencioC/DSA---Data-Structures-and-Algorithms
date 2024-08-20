a6171d654763d1fe9669e821924bfa85f35d2cfc
Inocencio Cordeiro <cordeiroturorias@gmail.com>
2024-08-20 18:35:29 +0100

    Simple_Product_BEE_1004

---
+++ src/main/java/org/example/basicExercises/Simple_Product_BEE_1004.java
@@ -0,0 +1,16 @@
+package org.example.basicExercises;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Simple_Product {
+    public static void main(String[] args) throws IOException {
+    Scanner read = new Scanner(System.in);
+
+    int A = read.nextInt();
+    int B = read.nextInt();
+
+    int PROD = A*B;
+        System.out.println("PROD = " + PROD);
+
+}
+}
