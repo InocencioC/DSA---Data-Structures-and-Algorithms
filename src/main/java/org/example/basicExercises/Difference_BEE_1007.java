b9c3a94c55e90711da13fe0a4e9f9caef92b6293
Inocencio Cordeiro <cordeiroturorias@gmail.com>
2024-08-23 20:47:19 +0100

    Calculate and print the difference of product

--- src/main/java/org/example/basicExercises/Difference_BEE_1007.java
+++ src/main/java/org/example/basicExercises/Difference_BEE_1007.java
@@ -1,4 +1,19 @@
 package org.example.basicExercises;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Difference_BEE_1007 {
+    public static void main(String[] args) throws IOException {
+
+    Scanner read = new Scanner(System.in);
+    int A = read.nextInt();
+    int B = read.nextInt();
+    int C = read.nextInt();
+    int D = read.nextInt();
+
+    int DIFERENCA = A * B - C * D;
+        System.out.println("DIFERENCA = " + DIFERENCA);
+}
+
+
 }
