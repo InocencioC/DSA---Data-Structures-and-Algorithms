4d2f9dfed81e5f2762b12eba219730e6a2872b8f
Inocencio Cordeiro <cordeiroturorias@gmail.com>
2024-08-27 23:32:26 +0100

    Salary

--- src/main/java/org/example/basicExercises/Salary_BEE_1008.java
+++ src/main/java/org/example/basicExercises/Salary_BEE_1008.java
@@ -1,19 +1,18 @@
 package org.example.basicExercises;
-import java.io.IOException;
 import java.util.Scanner;
+import java.io.IOException;
 
-public class Difference_BEE_1007 {
+public class Salary_BEE_1008 {
     public static void main(String[] args) throws IOException {
-
         Scanner read = new Scanner(System.in);
+
         int numberWorker = read.nextInt();
-        int amountHour = read.nextInt();
+        double amountHour = read.nextDouble();
         int hour = read.nextInt();
 
         double salary = hour * amountHour;
         System.out.println("NUMBER = " + numberWorker);
-        System.out.println(String.format("SALARY = U$ %.2f" ,salary));
+        System.out.printf("SALARY = U$ %.2f%n", salary);
+
 }
-
-
 }
