024d3b89bfc762fd1c98708d7e35407747a1f656
Inocencio Cordeiro <cordeiroturorias@gmail.com>
2024-12-05 00:19:10 +0100

    feat: finished linkedlist

--- src/main/java/org/example/LinkedList/ListaLigada.java
+++ src/main/java/org/example/LinkedList/ListaLigada.java
@@ -30,27 +30,54 @@ public class ListaLigada {
         this.tamanho = tamanho;
     }
 
-    public void adicionar(String novoValor){
+    public void adicionar(String novoValor) {
         Elemento novoElemento = new Elemento(novoValor);
-        if (this.primeiro == null && this.ultimo == null){
+        if (this.primeiro == null && this.ultimo == null) {
             this.primeiro = novoElemento;
             this.ultimo = novoElemento;
-        }else {
+        } else {
             this.ultimo.setProximo(novoElemento);
             this.ultimo = novoElemento;
         }
         this.tamanho++;
     }
-    public void remover(String novoValor){
 
-    }
-    public  Elemento get(int posicao){
+    public void remover(String ValorProcurado) {
+        Elemento anterior = null;
         Elemento atual = this.primeiro;
-        for (int i = 0; i<= posicao; i++){
-        if (atual.getProximo() !=null){
+        for (int i = 0; i < this.getTamanho(); i++) {
+            if (atual.getValor().equalsIgnoreCase(ValorProcurado)) {
+                if (this.tamanho == 1) {
+                    this.primeiro = null;
+                    this.ultimo = null;
+                } else if (atual == primeiro) {
+                this.primeiro = atual.getProximo();
+                    atual.setProximo(null);
+                } else if (atual == ultimo) {
+                    this.ultimo = anterior;
+                    anterior.setProximo(null);
+                } else {
+                    anterior.setProximo(atual.getProximo());
+                    atual = null;
+                }
+                this.tamanho--;
+                break;
+            }
+            anterior = atual;
             atual = atual.getProximo();
         }
+    }
+
+    public Elemento get(int posicao) {
+        Elemento atual = this.primeiro;
+        for (int i = 0; i <= posicao; i++) {
+            if (atual.getProximo() != null) {
+                atual = atual.getProximo();
+            }
         }
         return atual;
+
     }
 }
+
+
