86138025b48d5e4a1f1d914249922051851dc1d4
Inocencio Cordeiro <cordeiroturorias@gmail.com>
2024-12-09 22:21:40 +0100

    feat: comparing array with linkedlist

---
+++ src/main/java/org/example/LinkedList/Comparacao.java
@@ -0,0 +1,16 @@
+package org.example.LinkedList;
+
+import java.util.ArrayList;
+
+public class Comparacao {
+public static void main(String[] args) {
+    ListaLigada<Integer> lista = new ListaLigada<Integer>();
+    ArrayList<Integer> vetor = new ArrayList<Integer>();
+
+    int limite = 10;
+    long inicial = System.currentTimeMillis();
+    for(int i=0; i < limite; i++) {
+        vetor.add(i);
+    }
+  }
+}
